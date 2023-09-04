e13491ccebbb956fa897d28d422c72b649e41e08
jeonilshin <cloud.jeonilshin@gmail.com>
2023-09-04 20:59:52 +0900

    update html

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module beanstorage.com
 
-go 1.21.0
+go 1.21
 
 require (
 	dario.cat/mergo v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"log"
-    "net/http"
 	"context"
-	"os"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-    "github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -23,12 +23,12 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-    r := gin.Default()
-    r.Use(cors.Default())
+	r := gin.Default()
+	r.Use(cors.Default())
 
-    r.Static("/assets", "./assets")
-    r.LoadHTMLGlob("templates/*")
-    r.MaxMultipartMemory = 900 * 1024 * 1024
+	r.Static("/assets", "./assets")
+	r.LoadHTMLGlob("templates/*")
+	r.MaxMultipartMemory = 900 * 1024 * 1024
 
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
@@ -39,9 +39,9 @@ func main() {
 	client := s3.NewFromConfig(cfg)
 	uploader := manager.NewUploader(client)
 
-    r.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", gin.H{})
-    })
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
 
 	r.POST("/", func(c *gin.Context) {
 		file, err := c.FormFile("image")
@@ -65,7 +65,7 @@ func main() {
 			Bucket: aws.String(os.Getenv("AWS_BUCKET_NAME")),
 			Key:    aws.String(file.Filename),
 			Body:   f,
-			ACL:	"public-read",
+			ACL:    "public-read",
 		})
 
 		if uploadErr != nil {
@@ -89,23 +89,23 @@ func main() {
 			SecretKey  string `json:"secretKey"`
 			BucketName string `json:"bucketName"`
 		}
-	
+
 		if err := c.BindJSON(&formData); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
 			return
 		}
-	
+
 		content := "AWS_REGION=" + formData.Region + "\n" +
-				"AWS_ACCESS_KEY_ID=" + formData.AccessKey + "\n" +
-				"AWS_SECRET_ACCESS_KEY=" + formData.SecretKey + "\n" +
-				"AWS_BUCKET_NAME=" + formData.BucketName + "\n"
+			"AWS_ACCESS_KEY_ID=" + formData.AccessKey + "\n" +
+			"AWS_SECRET_ACCESS_KEY=" + formData.SecretKey + "\n" +
+			"AWS_BUCKET_NAME=" + formData.BucketName + "\n"
 
 		err := ioutil.WriteFile(".env", []byte(content), 0644)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save credentials"})
 			return
 		}
-	
+
 		c.JSON(http.StatusOK, gin.H{"message": "Credentials saved successfully"})
 	})
 	r.Run()
